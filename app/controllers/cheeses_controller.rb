45e215d7b2932062d7ff41f104dc7bbdcb26f5f9
Caroline Syokau <caroline.syokau@student.moringaschool.com>
2023-03-16 21:17:16 +0300

    completed assignment

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -4,12 +4,22 @@ class CheesesController < ApplicationController
   def index
     cheeses = Cheese.all
     render json: cheeses
+    # render json: cheeses, only: [:id, :name, :price, :is_best_seller]
+    render json: cheeses, except: [:created_at, :updated_at]
+   
   end
 
   # GET /cheeses/:id
   def show
     cheese = Cheese.find_by(id: params[:id])
     render json: cheese
+    # render json: cheese
+    # render json: cheese, except: [:created_at, :updated_at], methods: [:summary]
+    if cheese
+      render json: cheese, except: [:created_at, :updated_at], methods: [:summary]
+    else
+      render json: { error: 'Cheese not found' }, status: :not_found
+    end
   end
 
 end
